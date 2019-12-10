802280ee27ca684b32d7e18890d65a210f3ba735
Jasper Wan <wanjixiang819@hotmail.com>
2019-12-10 23:18:00 +0800

    [WIP] initial release

---
+++ Formula/bedrock-viz-dev.rb
@@ -0,0 +1,26 @@
+class BedrockVizDev < Formula
+    desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
+    homepage "https://github.com/bedrock-viz/bedrock-viz"
+    url "https://github.com/bedrock-viz/bedrock-viz/archive/v0.1.1.tar.gz"
+    sha256 ""
+    head "https://github.com/bedrock-viz/bedrock-viz.git"
+  
+    version "v0.1.1"
+  
+    depends_on "cmake" => :build
+    depends_on "libpng"
+    depends_on "libxml2"
+  
+    def install
+      mkdir "build" do
+        system "cmake", "..", *std_cmake_args
+        system "make"
+        system "make", "install"
+      end
+    end
+  
+    test do
+      # assert_match "Hello, World!", shell_output("#{bin}/helloworld", 255)
+      system "#{bin}/bedrock-viz"
+    end
+  end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Bedrock Viz Offical Tap
+
+Install bedrock-viz via:
+
+```
+brew install bedrock-viz/tap/bedrock-viz
+```
+
+
