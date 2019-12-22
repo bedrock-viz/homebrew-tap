e5ff492e505b74bc674d78fce3e7271b97292e7d
Wan Jixiang <wanjixiang819@hotmail.com>
2019-12-22 10:38:39 +0800

    Fix wrong url and shasum

--- Formula/bedrock-viz.rb
+++ Formula/bedrock-viz.rb
@@ -1,8 +1,8 @@
 class BedrockViz < Formula
     desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
     homepage "https://github.com/bedrock-viz/bedrock-viz"
-    url "https://github.com/bedrock-viz/bedrock-viz/archive/v0.1.1.tar.gz"
-    sha256 ""
+    url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.1/bedrock-viz_v0.1.1.tar.gz"
+    sha256 "80a0772c5e43d9f62f1830e6e012f9d7cfca9da7aee68f0dbb5e6918f4d253ef"
     head "https://github.com/bedrock-viz/bedrock-viz.git"
   
     version "v0.1.1"
