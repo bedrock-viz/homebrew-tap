09bbfd83dc3db28629c536d7d61032057d07de26
jasper-wan <wanjixiang819@hotmail.com>
2020-06-29 14:42:32 +0800

    Update bedrock-viz.rb

--- Formula/bedrock-viz.rb
+++ Formula/bedrock-viz.rb
@@ -2,7 +2,7 @@ class BedrockViz < Formula
     desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
     homepage "https://github.com/bedrock-viz/bedrock-viz"
     url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.2/bedrock-viz_v0.1.2_macOS.tar.gz"
-    sha256 "bd710acabc7588d97f5e2a7abd5be36352952952875380ba0a5e9b1980eeaabf"
+    sha256 "520128b816c3a6d81f0945589b1ac6d36842e5ae9f910fac6818c3ee8863254a"
     head "https://github.com/bedrock-viz/bedrock-viz.git"
   
     version "v0.1.2"
