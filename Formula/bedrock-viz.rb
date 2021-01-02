0a3f9c5a364b7035249543ea0eea309216f79197
Chris Abbey <522147+cabbey@users.noreply.github.com>
2021-01-01 20:41:00 -0800

    fix sha256 for corrected tarball
    previous tarball was incomplete

--- Formula/bedrock-viz.rb
+++ Formula/bedrock-viz.rb
@@ -2,7 +2,7 @@ class BedrockViz < Formula
     desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
     homepage "https://github.com/bedrock-viz/bedrock-viz"
     url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.3/bedrock-viz_v0.1.3_macOS.tar.gz"
-    sha256 "cbeeb9df7305f9c5dc14f040917789e44a4cf7b35b92fc5e4bd70b9f38ce3523"
+    sha256 "2b69797c4814877ef5dcdd8174ae61482cb0127fab94458233986783600829c3"
     head "https://github.com/bedrock-viz/bedrock-viz.git"
   
     version "v0.1.3"
