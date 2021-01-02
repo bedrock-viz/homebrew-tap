f7ac721613dd23351ce885971d3669ada5fedb20
Chris Abbey <522147+cabbey@users.noreply.github.com>
2021-01-01 21:37:06 -0800

    update to sha for tarball that might actually build
    yet more missing files.

--- Formula/bedrock-viz.rb
+++ Formula/bedrock-viz.rb
@@ -2,7 +2,7 @@ class BedrockViz < Formula
     desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
     homepage "https://github.com/bedrock-viz/bedrock-viz"
     url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.3/bedrock-viz_v0.1.3_macOS.tar.gz"
-    sha256 "2b69797c4814877ef5dcdd8174ae61482cb0127fab94458233986783600829c3"
+    sha256 "d335cf37bdb639df448ddc5c53034680aa1d3cbba78d6906c36a73e6535061e9"
     head "https://github.com/bedrock-viz/bedrock-viz.git"
   
     version "v0.1.3"
