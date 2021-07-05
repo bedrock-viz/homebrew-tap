846e35a7fc6551d1de1bebf4a579a237a423ae28
Chris Abbey <cabbey@cabbey.net>
2021-07-04 19:06:12 -0700

    update to v0.1.4

--- Formula/bedrock-viz.rb
+++ Formula/bedrock-viz.rb
@@ -1,11 +1,11 @@
 class BedrockViz < Formula
     desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
     homepage "https://github.com/bedrock-viz/bedrock-viz"
-    url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.3/bedrock-viz_v0.1.3_macOS.tar.gz"
-    sha256 "d335cf37bdb639df448ddc5c53034680aa1d3cbba78d6906c36a73e6535061e9"
+    url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.4/bedrock-viz_v0.1.4_macOS.tar.gz"
+    sha256 "4cd6a0f8105e2187518f57bcffcd44ec88a6d613c913431d039bff97e83baf28"
     head "https://github.com/bedrock-viz/bedrock-viz.git"
   
-    version "v0.1.3"
+    version "v0.1.4"
   
     depends_on "cmake" => :build
     depends_on "libpng"
