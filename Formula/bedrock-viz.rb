6a8c121e1f907eef034fcea28b2fbece89334905
Chris Abbey <cabbey@cabbey.net>
2022-02-13 15:03:32 -0800

    0.1.7 is live now

--- Formula/bedrock-viz.rb
+++ Formula/bedrock-viz.rb
@@ -1,11 +1,11 @@
 class BedrockViz < Formula
     desc "Minecraft Bedrock Edition World Visualization & Reporting Tool with Web App"
     homepage "https://github.com/bedrock-viz/bedrock-viz"
-    url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.6/bedrock-viz_v0.1.6_macOS.tar.gz"
-    sha256 "07d01b531ce6ab73de0037a144b57be48844c566a13a14c94bb5dca6aac92899"
+    url "https://github.com/bedrock-viz/bedrock-viz/releases/download/v0.1.7/bedrock-viz_v0.1.7_macOS.tar.gz"
+    sha256 "81e45d4cbaf78462818c73c3ea324b260f5e4faa282fab887843667493718374"
     head "https://github.com/bedrock-viz/bedrock-viz.git"
   
-    version "v0.1.6"
+    version "v0.1.7"
   
     depends_on "cmake" => :build
     depends_on "libpng"
